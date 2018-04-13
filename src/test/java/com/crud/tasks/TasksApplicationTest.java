27ccf86d1d7078ffda3619b239472263d6c2417b
“marjancz <mariusz@janczyk.com.pl>
2018-04-13 18:49:17 +0200

    work 15.2

--- src/test/java/com/crud/tasks/TasksApplicationTest.java
+++ src/test/java/com/crud/tasks/TasksApplicationTest.java
@@ -7,10 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TasksApplicationTests {
+public class TasksApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
