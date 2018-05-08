b6d408c859e378947f7d8469d6cac8eb1b197d2f
“marjancz <mariusz@janczyk.com.pl>
2018-05-08 18:07:00 +0200

    work 18.3

--- src/main/java/com/crud/tasks/domain/TrelloAttachmentsByTypeDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloAttachmentsByTypeDto.java
@@ -6,20 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class TrelloAttachmentsByTypeDto {
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("trello")
+    private TrelloDto trello;
 }
